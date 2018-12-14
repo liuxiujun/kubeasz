b173864a8d245982379db299d7db0d356be9cd30
root <root@k8s-v1-sy-master-01.mxnavi.com>
2018-12-14 16:34:48 +0800

    增加service测试

---
+++ yaml-demo/service-connect-test/dns-connect/run-load.sh
@@ -0,0 +1,7 @@
+set -x
+if [ "$1" == "" ];then
+  echo "输入namespace"
+  exit 1
+fi
+FORTIO_POD=$(kubectl get pod -n $1 | grep fortio | awk '{ print $1 }')
+kubectl exec -n $1 -it $FORTIO_POD  -c fortio /usr/local/bin/fortio -- load -c 10 -qps 0 -n 100 -loglevel Warning http://service-c.demo.k8s-project.mxnavi.com/
